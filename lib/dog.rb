30bb145b0c64ad2311e130aa83cbc916958d2962
thakaj <taulant.hakaj@scranton.edu>
2021-08-04 11:31:40 -0400

    commit tree 162888485110213fef7b7d1d92706240fd33ef7b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        self.save
+    end
+    def save
+        @@all<< self
+    end
+
+    def self.all
+        @@all
+    end
+    def self.print_all
+        @@all.each {|name| puts name.name} 
+    end
+    def self.clear_all
+        @@all.clear
+    end
+
+end
